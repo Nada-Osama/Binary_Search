7c79b3c2830e5b1c6caf3ed83b37433466be48c5
Nada Osama <nosamaosman@gmail.com>
2019-07-07 21:19:11 +0200

    initial commit for binary search
    Co-Authored-By: Ahmed Hafez <ahmedhafez1@users.noreply.github.com>

---
+++ BinarySearch/Binarysearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Binarysearch<T>
+    {
+        public String Search (T[] list, T item)
+        {
+            Comparer<T> comparer = Comparer<T>.Default;
+            int low = 0, high = list.Length - 1;
+            while ( low <= high )
+            {
+                int mid = (low + high) / 2;
+                if ((list[mid]).Equals(item))
+                    return mid.ToString();
+                if (comparer.Compare(list[mid], item) > 0)
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            return "Not found in this list!";
+        }
+    }
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the item you are searching for: ");
+            int my_item = int.Parse(Console.ReadLine());
+            int[] my_list = { 1, 3, 5, 7, 9 };
+            Binarysearch<int> binsrch = new Binarysearch<int>();
+            Console.WriteLine( binsrch.Search(my_list, my_item) );
+        }
+    }
+}
